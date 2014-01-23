0d486acdd74470432f568e988197153283bcf357
sallywsimpson <ssimpson2014@kellogg.northwestern.edu>
2014-01-23 16:57:04 -0600

    Completed Homework

--- monopoly.rb
+++ monopoly.rb
@@ -17,21 +17,24 @@ puts cheap_property[:price]
 
 #Q2. How much does it cost to buy Boardwalk?
 #A2: Your code goes here:
+puts rich_property[:price]
 
 
 
 #Q3. How much is rent for Baltic?
 #A3: Your code goes here:
-
+puts cheap_property[:rent]
 
 
 #Q4. How much does it cost to buy New York Ave?
 #A4: Your code goes here:
-
+new_york_ave = more_properties[2]
+puts new_york_ave[:price]
 
 
 #Q5: How many "good" special squares are there?
 #A5: Your code goes here:
+puts special_squares[:good].count
 
 
 
